1d85aab766b69b3f50a326b55f40855bec661f21
Dominic789654 <47224289+Dominic789654@users.noreply.github.com>
2025-07-15 11:22:42 -0400

    update

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -13,7 +13,7 @@ export const News: React.FC = () => {
     {
       id: 1,
       date: 'July 2025 - Jan 2026',
-      content: 'Visiting Student at NYU with Prof. Eunsol Choi</a>',
+      content: 'Visiting Student at NYU with Prof. Eunsol Choi',
       isNew: true
     },
     {
